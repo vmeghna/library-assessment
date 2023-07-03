1264142c56f453c684852e3123a5cb076d0510e5
vmeghna <shrimeghna97@gmail.com>
2023-07-03 17:23:17 +0530

    final without js

---
+++ script.js
@@ -0,0 +1,15 @@
+// 'use strict'
+
+// let libraryForm = document.getElementById('libraryForm');
+
+// libraryForm.addEventListener('submit', (e) => {
+
+//     e.preventDefault();
+
+//     let name = document.getElementById("bookName").value;
+//     let author = document.getElementById("author").value;
+
+//     let fiction = document.getElementById('fiction');
+//     let programming = document.getElementById('programming');
+//     let science = document.getElementById('science');
+//     let type;
